2ca529e15d704a903b68591b7e60a1579cae6a03
mahshad golzarian <mahshad@mahshads-MacBook-Pro.local>
2025-05-03 23:06:15 -0400

    password genrator

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -7,3 +7,4 @@ while count < 12 :
     random_string += random.choice(string.ascii_lowercase)
     count += 1
 print(random_string)
+# I tried to figure it out on my own, but I honestly didn't get further than figuring out it's a while loop.. I'm still not very skilled when it comes to For and While loops...so I looked at the solution you put on canvas
